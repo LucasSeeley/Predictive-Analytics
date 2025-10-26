12de2e9cb1fa4260a7ee7aba9459c171daa65474
Lucas Seeley <lucas.a.seeley@gmail.com>
2025-10-26 13:35:43 -0400

    cfb_plays model

--- models/foundational/cfb_plays.sql
+++ models/foundational/cfb_plays.sql
@@ -2,6 +2,23 @@ MODEL (
     name cfb.cfb_plays,
     kind FULL
 );
-SELECT
-    *
+SELECT DISTINCT
+    id AS play_id,
+    drive_id,
+    game_id,
+    drive_number,
+    play_number,
+    period AS quarter,
+    clock_minutes,
+    clock_seconds,
+    offense_timeouts,
+    defense_timeouts,
+    yardline,
+    yards_to_goal,
+    down,
+    distance AS distance_to_first_down,
+    yards_gained,
+    scoring,
+    play_type,
+    play_text
 FROM cfb.cfb_plays_source
