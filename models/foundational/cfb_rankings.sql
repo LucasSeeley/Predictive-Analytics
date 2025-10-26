332a12c77834c5e895d59ac65f4e700d6b70b438
Lucas Seeley <lucas.a.seeley@gmail.com>
2025-10-26 13:13:47 -0400

    roster & rankings models

--- models/foundational/cfb_rankings.sql
+++ models/foundational/cfb_rankings.sql
@@ -3,5 +3,12 @@ MODEL (
     kind FULL
 );
 SELECT
-    *
+    season,
+    season_type,
+    week,
+    team_id,
+    poll,
+    rank,
+    first_place_votes,
+    points
 FROM cfb.cfb_rankings_source
--- models/foundational/cfb_roster.sql
+++ models/foundational/cfb_roster.sql
@@ -3,5 +3,17 @@ MODEL (
     kind FULL
 );
 SELECT
-    *
+    id AS player_id,
+    season,
+    CONCAT(first_name, ' ', last_name) AS player_name,
+    team AS team_name,
+    height,
+    weight,
+    jersey AS jersey_number,
+    position,
+    home_city,
+    home_state,
+    home_country,
+    home_latitude,
+    home_longitude
 FROM cfb.cfb_roster_source
