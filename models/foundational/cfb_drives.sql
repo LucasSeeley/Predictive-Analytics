31fc18a164aa74d6fd7e76ad08961d7cd0a729cf
Lucas Seeley <lucas.a.seeley@gmail.com>
2025-10-26 13:32:25 -0400

    cfb_drives model

--- models/foundational/cfb_drives.sql
+++ models/foundational/cfb_drives.sql
@@ -2,6 +2,32 @@ MODEL (
     name cfb.cfb_drives,
     kind FULL
 );
-SELECT
-    *
+SELECT DISTINCT
+    id AS drive_id,
+    game_id,
+    year AS season,
+    week,
+    offense AS offense_team_name,
+    defense AS defense_team_name,
+    drive_number,
+    scoring,
+    drive_result,
+    plays,
+    yards,
+    start_period,
+    end_period,
+    start_yardline,
+    end_yardline,
+    start_yards_to_goal,
+    end_yards_to_goal,
+    start_offense_score,
+    start_defense_score,
+    end_offense_score,
+    end_defense_score,
+    start_time_minutes,
+    start_time_seconds,
+    end_time_minutes,
+    end_time_seconds,
+    elapsed_minutes,
+    elapsed_seconds
 FROM cfb.cfb_drives_source
